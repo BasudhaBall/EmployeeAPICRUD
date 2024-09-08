fb6083e991ab7e5a0cdc30dcd870e4addd43ab5f
BasudhaBall <basudha695bc.com>
2024-09-08 21:36:22 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,65 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@PostMapping("/employees")
+	public String createNewEmployee(@RequestBody Employee employee) {
+		employeeRepository.save(employee);
+
+		return "Emlpoyee Created in Database";
+	}
+
+	@GetMapping("/employees")
+	public ResponseEntity<List<Employee>> getAllEmployees() {
+		List<Employee> empList = new ArrayList<>();
+		employeeRepository.findAll().forEach(empList::add);
+		return new ResponseEntity<List<Employee>>(empList, HttpStatus.OK);
+	}
+
+	@PutMapping("/employees/{empid}")
+	public String updateEmployeeById(@PathVariable long empid, @RequestBody Employee employee) {
+		Optional<Employee> emp = employeeRepository.findById(empid);
+		if (emp.isPresent()) {
+			Employee existEmp = emp.get();
+			existEmp.setEmpAge(employee.getEmpAge());
+			existEmp.setEmpCity(employee.getEmpCity());
+			existEmp.setEmpName(employee.getEmpName());
+			existEmp.setEmpSalary(employee.getEmpSalary());
+			employeeRepository.save(existEmp);
+			return "Employee Details against Id " + empid + " updated";
+		} else {
+			return "Employee Details does not exist for empid " + empid;
+		}
+	}
+
+	@DeleteMapping("/employees/{empId}")
+	public String deleteEmployeeByEmpId(@PathVariable Long empId) {
+		employeeRepository.deleteById(empId);
+		return "Employee Deleted Successfully";
+	}
+
+}
---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,89 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+
+@Table(name = "employee")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "empid")
+	private long empId;
+
+	@Column(name = "empname")
+	private String empName;
+
+	@Column(name = "empsalary")
+	private Float empSalary;
+
+	@Column(name = "empage")
+	private String empAge;
+
+	@Column(name = "empcity")
+	private String empCity;
+
+	public long getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(long empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public Float getEmpSalary() {
+		return empSalary;
+	}
+
+	public void setEmpSalary(Float empSalary) {
+		this.empSalary = empSalary;
+	}
+
+	public String getEmpAge() {
+		return empAge;
+	}
+
+	public void setEmpAge(String empAge) {
+		this.empAge = empAge;
+	}
+
+	public String getEmpCity() {
+		return empCity;
+	}
+
+	public void setEmpCity(String empCity) {
+		this.empCity = empCity;
+	}
+
+	public Employee() {
+	}
+
+	public Employee(long empId, String empName, Float empSalary, String empAge, String empCity) {
+		this.empId = empId;
+		this.empName = empName;
+		this.empSalary = empSalary;
+		this.empAge = empAge;
+		this.empCity = empCity;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSalary=" + empSalary + ", empAge=" + empAge
+				+ ", empCity=" + empCity + "]";
+	}
+	
+
+}
